53fc7e579f145cb01ccd0ad9ad9f37f0b974be65
Thomas Micol <thomasmicol@gmail.com>
2020-12-19 22:04:13 +1300

    started logic for the calculation of the projection

--- App/ClientApp/src/app/home/home.component.ts
+++ App/ClientApp/src/app/home/home.component.ts
@@ -13,13 +13,29 @@ export class HomeComponent {
   Duration : number = 0;
 
   HasBeenCalculated: boolean = false;
+  DataSet: any[];
 
   StartIncomeChange(event) {
     console.log(this);
   }
 
   calculate() {
+    let iterationCount = 1;
+    let income = this.StartIncome;
+    
 
+    while (iterationCount <= this.Duration) {
+      let split = income * (this.InvestPercentage / 100);
+      let spentSplit = income - split;
+
+      let newItem = {
+        Year: iterationCount,
+        Income: income,
+        Split: spentSplit + " / " + split ,
+        Invested: ,
+        YearInvestmentGrowth: 
+      };
+    }
     this.HasBeenCalculated = true;
   }
 
